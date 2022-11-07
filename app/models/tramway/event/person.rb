c31ae907bf9d65df1ee9cb4a8025349a1c054b34
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-11-07 22:58:39 +0400

    Implement partakings association

--- app/models/tramway/event/person.rb
+++ app/models/tramway/event/person.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Tramway::Event::Person < ::Tramway::Core::ApplicationRecord
+  has_many :partakings, class_name: 'Tramway::Event::Partaking'
+
   mount_uploader :photo, PhotoUploader
 end
