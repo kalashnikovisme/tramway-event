e6d4c09468845de77f933b031e0b03dcdfad6edc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-09 18:39:58 +0400

    Update initializers

--- config/initializers/tramway.rb
+++ config/initializers/tramway.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
-  Tramway.set_available_models(Tramway::Event::Event,
-                               Tramway::Event::ParticipantFormField,
-                               Tramway::Event::Participant,
-                               Tramway::Event::Section,
-                               Tramway::Event::Person,
-                               Tramway::Event::Partaking,
-                               Tramway::Event::Place,
-                               Tramway::Event::Action,
-                               project: :event)
+  Tramway.set_available_models(
+    [
+      Tramway::Event::Event,
+      Tramway::Event::ParticipantFormField,
+      Tramway::Event::Participant,
+      Tramway::Event::Section,
+      Tramway::Event::Person,
+      Tramway::Event::Partaking,
+      Tramway::Event::Place,
+      Tramway::Event::Action
+    ],
+    project: :event
+  )
   Tramway.set_notificable_queries new_participants: lambda { |current_user|
     Tramway::Event::Participant.where(participation_state: :requested).send "#{current_user.role}_scope", current_user.id
   }
